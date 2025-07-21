373b2c4bd8ebec0191e95586ac5199b209ea73c3
Miguel Amores <87871782+44mgl@users.noreply.github.com>
2025-07-21 19:43:24 -0300

    Adicionando o Arquivo JavaScript

---
+++ script.js
@@ -0,0 +1,20 @@
+let nomeDoHeroi = "mgl"
+let qtdExp = 10001
+
+if(qtdExp <= 1000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Ferro!")
+}else if(qtdExp >= 1001 && qtdExp <= 2000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Bronze!")
+}else if(qtdExp >= 2001 && qtdExp <= 5000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Prata!")
+}else if(qtdExp >= 5001 && qtdExp <= 7000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Ouro!")
+}else if(qtdExp >= 7001 && qtdExp <= 8000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Platina!")
+}else if(qtdExp >= 8001 && qtdExp <= 9000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Ascendente!")
+}else if(qtdExp >= 9001 && qtdExp <= 10000){
+console.log("O nivel do Herói " + nomeDoHeroi +" é Imortal!")
+}else{
+ console.log("O nivel do Herói " + nomeDoHeroi +" é Radiante!")
+}
